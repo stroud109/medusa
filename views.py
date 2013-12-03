d5841be948dceffe015b6751f3dc13e2ab6b49e3
Stephanie Stroud <stephanie.marie.stroud@gmail.com>
2013-12-02 22:59:49 -0800

    Customized 404 page

--- views.py
+++ views.py
@@ -643,6 +643,11 @@ def deactivate_accout():
     pass
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html"), 404
+
+
 if __name__ == "__main__":
     app.run(
         debug=True,
